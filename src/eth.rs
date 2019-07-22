a66cb235d788dba12f1ec9dd6e92917f1dcf591c
jayphbee <jayphbee@163.com>
2019-07-22 20:58:40 +0800

    eth_from_mnemonic

--- src/eth.rs
+++ src/eth.rs
@@ -1,6 +1,7 @@
 use std::os::raw::c_char;
 use std::ffi::{ CStr, CString };
 use std::ptr;
+use std::mem::{self, MaybeUninit};
 
 use ethabi::{ Contract, Token };
 use primitive_types;
@@ -10,6 +11,8 @@ use hex::{ encode, decode };
 
 use tiny_hderive::bip32::ExtendedPrivKey;
 use bip39::{Mnemonic, MnemonicType, Language, Seed};
+
+use ethsign::{SecretKey, PublicKey};
 
 #[repr(C)]
 pub struct eth_tx_meta {
@@ -22,6 +25,7 @@ pub struct eth_tx_meta {
     priv_key: *const c_char,
     chain_id: u8,
 }
+
 
 #[no_mangle]
 pub extern "C" fn build_signed_eth_tx(tx: *const eth_tx_meta) -> *mut c_char {
@@ -83,6 +87,58 @@ pub extern "C" fn dealloc_rust_cstring(cstring: *mut c_char) {
 }
 
 #[no_mangle]
+pub extern "C" fn eth_from_mnemonic(mnemonic: *const c_char, language: *const c_char, address: *mut *mut c_char, priv_key: *mut *mut c_char, master_seed: *mut *mut c_char) -> i32 {
+    let mnemonic = unsafe {
+        CStr::from_ptr(mnemonic).to_str().unwrap()
+    };
+
+    let language = unsafe {
+        let lan = CStr::from_ptr(language).to_str().unwrap();
+        if lan == "english" {
+            Language::English
+        } else if lan == "chinese_simplified" {
+            Language::ChineseSimplified
+        } else if lan == "chinese_traditional" {
+            Language::ChineseTraditional
+        } else {
+            return -1;
+        }
+    };
+
+    let seed = Seed::new(&Mnemonic::from_phrase(mnemonic, language).unwrap(), "");
+    let ext = ExtendedPrivKey::derive(seed.as_bytes(), "m/44'/60'/0'/0/0").unwrap();
+    let privte_key = SecretKey::from_raw(&ext.secret()).unwrap();
+
+    unsafe {
+        *address = CString::new(encode(privte_key.public().address())).unwrap().into_raw();
+        *priv_key = CString::new(encode(ext.secret())).unwrap().into_raw();
+        *master_seed = CString::new(encode(seed)).unwrap().into_raw();
+    }
+
+    return 0;
+}
+
+#[no_mangle]
+pub extern "C" fn eth_generate(strength: u32, language: *const c_char, address: *mut *mut c_char, priv_key: *mut *mut c_char, master_seed: *mut *mut c_char, mnemonic: *mut *mut c_char) -> i32 {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub extern "C" fn eth_sign_raw_transaction(nonce: *const c_char, to: *const c_char) {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub extern "C" fn eth_select_wallet(master_seed: *const c_char, index: u32, address: *mut *mut c_char, priv_key: *mut *mut c_char) -> i32 {
+    unimplemented!();
+}
+
+#[no_mangle]
+pub extern "C" fn get_public_key_by_mnemonic(mnemonic: *const c_char, language: *const c_char) {
+    unimplemented!();
+}
+
+#[no_mangle]
 pub extern "C" fn token_balance_call_data(addr: *const c_char) -> *mut c_char {
     unsafe {
         let addr = String::from_utf8_lossy(CStr::from_ptr(addr).to_bytes()).to_string();
@@ -120,6 +176,29 @@ pub extern "C" fn token_transfer_call_data(addr_to: *const c_char, value: *const
 mod test {
     use super::*;
     use hex::{encode, decode};
+    use std::ptr;
+
+    #[test]
+    fn test_eth_from_mnemonic() {
+        let m = CString::new("lunar exercise inside defense accuse reopen symbol oak milk top chunk axis").unwrap();
+        let lan = CString::new("english").unwrap();
+
+        let addr = MaybeUninit::<*mut c_char>::uninit().as_mut_ptr();
+        let master = MaybeUninit::<*mut c_char>::uninit().as_mut_ptr();
+        let priv_key = MaybeUninit::<*mut c_char>::uninit().as_mut_ptr();
+
+        unsafe {
+            eth_from_mnemonic(m.into_raw(), lan.into_raw(), addr, priv_key, master);
+
+            let addr = CString::from_raw(*addr as *mut c_char);
+            let master = CString::from_raw(*master as *mut c_char);
+            let priv_key = CString::from_raw(*priv_key as *mut c_char);
+
+            assert_eq!(addr.to_str().unwrap(), "151ab25145928bf6dd013f6de3446fd6d67ad8c9");
+            assert_eq!(master.to_str().unwrap(), "065a6b481570379561574676ad61c96caf3e05ebd1d79c712f3dec938be73bbffd6ad02bd62ad90d1a9d6f00551134d480e1166fa8d0be2cc4d7132dd66a282c");
+            assert_eq!(priv_key.to_str().unwrap(), "ff33ff993b1782d16427276a9cc966b8b995c118f0182243152a882b8d3e3faf");
+        }
+    }
 
     #[test]
     fn mnemonic_to_privtekey() {
